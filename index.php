e8928cf70954f6d296a91285a0c330828b2ff715
Butterfly <112062269+13utterflyy@users.noreply.github.com>
2024-12-20 19:07:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hacked By 13utterfly</title>
+<link rel="icon" type="image/x-icon" href="https://c4.wallpaperflare.com/wallpaper/429/98/385/anime-girl-brunette-butterfly-wallpaper-preview.jpg">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Hacked By 13utterfly">
+	<meta name="description" content="Hacked By 13utterfly | AnonSec Team">
+<style>
+	@import url('https://fonts.googleapis.com/css?family=Acme&display=swap');
+	body{
+		font-family: 'Acme';
+		background-color: black;
+		color: white;
+		text-align: center;
+		margin-top: 8%;
+		width: auto;
+	}
+	h1{
+		margin-top: 0%;
+		font-size: 2.8vw;
+	}
+</style>
+</head>
+<body>
+	<img src="https://c4.wallpaperflare.com/wallpaper/429/98/385/anime-girl-brunette-butterfly-wallpaper-preview.jpg" width="400">
+	<h1>Hacked By 13utterfly</h1>
+	<i>
+	<font color="red" size="4">Hello admin, 13utterfly was here</font><br><br><br>
+  	<font color="white" size="4">In every time , every world , every universe, I HAVE COME!<br>FUCK YOUR SECURITY SITE!!</font>
+	<br>
+	</i>
+</html>
